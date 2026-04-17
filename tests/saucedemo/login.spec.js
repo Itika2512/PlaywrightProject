0e32a715a1d2ce168573d20109ad1dbe81615fe7
Itika2512 <itika.1216@gmail.com>
2026-04-17 16:45:19 +0530

    remove the product

--- tests/saucedemo/login.spec.js
+++ tests/saucedemo/login.spec.js
@@ -23,19 +23,19 @@ await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
 await page.screenshot({ path: './tests/screenshots/saucedemo.png' });
 
 //Checkout the product
-await page.locator('[data-test="shopping-cart-link"]').click();
-await page.getByRole('button', { name: 'Checkout' }).click();
-await page.getByPlaceholder('First Name').fill('John');
-await page.getByPlaceholder('Last Name').fill('Doe');
-await page.getByPlaceholder('Postal Code').fill('12345');
-await page.getByRole('button', { name: 'Continue' }).click();
-await page.getByRole('button', { name: 'Finish' }).click();
+// await page.locator('[data-test="shopping-cart-link"]').click();
+// await page.getByRole('button', { name: 'Checkout' }).click();
+// await page.getByPlaceholder('First Name').fill('John');
+// await page.getByPlaceholder('Last Name').fill('Doe');
+// await page.getByPlaceholder('Postal Code').fill('12345');
+// await page.getByRole('button', { name: 'Continue' }).click();
+// await page.getByRole('button', { name: 'Finish' }).click();
 
 
 //remove the product
-// await page.getByRole('button', { name: 'Remove', exact: true }).click();
-// await page.getByRole('button', { name: 'Open Menu' }).click();
-// await page.getByRole('link', { name: 'Logout' }).click();
+ await page.getByRole('button', { name: 'Remove', exact: true }).click();
+ await page.getByRole('button', { name: 'Open Menu' }).click();
+ await page.getByRole('link', { name: 'Logout' }).click();
 
 
 
